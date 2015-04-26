642840bda024f3b811727a52fa4333300271f3f3
johnny schmidt <sk8rdie42@gmail.com>
2015-04-25 17:20:13 -0700

    added wireshark file to test

--- shp_test.js
+++ shp_test.js
@@ -1,7 +1,7 @@
 var SHP = require("./SimpleHttpParser");
 //var parser = new SHP.SimpleHttpParser("packets/024.019.225.228.13836-096.126.115.201.00080");
 var parser = new SHP.SimpleHttpParser("test-data/packets/096.126.115.201.00080-024.019.225.228.13836");
-
+var wiresharkparser = new SHP.SimpleHttpParser("test-data/wireshark.raw");
 
 parser.on('done', function(obj) {
     console.log(JSON.stringify(parser, null, 2));
